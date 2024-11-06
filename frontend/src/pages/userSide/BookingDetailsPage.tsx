cd14c96fff04aae1dd0a192c1a7f2f8e7c18050c
MithilajHassan <mithilajmt7@gmail.com>
2024-11-06 20:58:44 +0530

    6-11-24

--- frontend/src/pages/userSide/BookingDetailsPage.tsx
+++ frontend/src/pages/userSide/BookingDetailsPage.tsx
@@ -4,13 +4,15 @@ import UserHeader from "../../components/users/UserHeader"
 import { useSelector } from "react-redux"
 import { RootState } from "@/store"
 import { useParams } from "react-router-dom"
+import GuestDetails from "../../components/users/GuestDetails"
+import { Button } from "../../components/ui/button"
 
 
 export default function BookingDetailsPage() {
     const { bookings } = useSelector((state: RootState) => state.bookings)
     const { id } = useParams()
-    const bookingData = bookings?.find((value)=>value._id == id)
-    
+    const bookingData = bookings?.find((value) => value._id == id)
+
     return (
         <>
             <UserHeader />
@@ -20,23 +22,29 @@ export default function BookingDetailsPage() {
                     {typeof bookingData?.resortId != 'string' && typeof bookingData?.roomId != 'string' &&
                         <BookingDetails checkoutDetails={bookingData!} resort={bookingData?.resortId!} room={bookingData?.roomId!} />
                     }
-                    {/* {typeof bookingData?.roomId != 'string' &&
+                    {typeof bookingData?.roomId != 'string' &&
                         <Bill checkoutDetails={bookingData!} room={bookingData?.roomId!} />
-                    } */}
+                    }
                 </div>
 
                 <div className="flex flex-col md:flex-row space-y-4 md:space-x-4 md:space-y-0 px-6 ">
 
-                    {/* <GuestForm /> */}
+                    <GuestDetails bookingDetails={bookingData!} />
 
                     <div className="w-80">
+                        <p className="py-4 text-center md:text-lg font-semibold">Booking Status :
+                            <span className={`${bookingData?.status === 'Cancelled' ? 'text-red-700' : 'text-blue-700'} ms-1 font-bold`}>
+                                {bookingData?.status}
+                            </span>
+                        </p>
 
+                        <div className="flex justify-center w-full">
+                            <Button className="w-52 bg-red-600 hover:bg-red-400 text-md" size={'lg'}>Cancell</Button>
+                        </div>
                     </div>
-
+                    
                 </div>
-
             </div>
         </>
-
     )
 }
